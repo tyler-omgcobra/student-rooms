2bd7cf630ca912b6a61f81992b4c47ee6f18e512
Tyler Smith <tyler@omgcobra.org>
2021-11-27 23:47:03 -0500

    Use secure web sockets

--- src/jsMain/kotlin/org/omgcobra/api/util.kt
+++ src/jsMain/kotlin/org/omgcobra/api/util.kt
@@ -34,7 +34,7 @@ suspend inline fun <reified T> post(body: Any, vararg paths: String): T =
 suspend fun websocket(vararg path: String, block: suspend DefaultClientWebSocketSession.() -> Unit) = jsonClient.webSocket(request = {
   url {
     takeFrom(endpoint)
-    protocol = URLProtocol.WS
+    protocol = URLProtocol.WSS
     path(*path)
   }
 }, block = block)
